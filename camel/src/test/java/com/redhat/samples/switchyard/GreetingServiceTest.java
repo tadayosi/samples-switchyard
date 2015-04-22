fe4bda4aefb5576d3f57943f45566134a7fd91c2
Tadayoshi Sato <tadayosi@gmail.com>
2015-04-22 11:43:46 +0900

    Added unit test to "camel" sample

--- camel/src/test/java/com/redhat/samples/switchyard/GreetingServiceTest.java
+++ camel/src/test/java/com/redhat/samples/switchyard/GreetingServiceTest.java
@@ -5,29 +5,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.switchyard.component.http.endpoint.StandaloneEndpointPublisher;
-import org.switchyard.component.test.mixins.http.HTTPMixIn;
-import org.switchyard.test.BeforeDeploy;
+import org.switchyard.component.test.mixins.cdi.CDIMixIn;
+import org.switchyard.test.Invoker;
+import org.switchyard.test.ServiceOperation;
 import org.switchyard.test.SwitchYardRunner;
 import org.switchyard.test.SwitchYardTestCaseConfig;
 
 @RunWith(SwitchYardRunner.class)
-@SwitchYardTestCaseConfig(config = SwitchYardTestCaseConfig.SWITCHYARD_XML, mixins = { HTTPMixIn.class })
+@SwitchYardTestCaseConfig(config = SwitchYardTestCaseConfig.SWITCHYARD_XML, mixins = { CDIMixIn.class })
 public class GreetingServiceTest {
 
-    private static final String ENDPOINT_URL = "http://localhost:18080/sample/greeting";
-
-    private HTTPMixIn httpMixIn;
-
-    @BeforeDeploy
-    public void setProperties() {
-        System.setProperty(StandaloneEndpointPublisher.DEFAULT_PORT_PROPERTY, "18080");
-    }
+    @ServiceOperation("GreetingService")
+    private Invoker service;
 
     @Test
-    public void hello() {
-        String name = getClass().getSimpleName();
-        String response = httpMixIn.postString(ENDPOINT_URL, name);
-        assertThat(response, is(String.format("Hello, %s!", name)));
+    public void route() {
+        String result = service.operation("").sendInOut("Test").getContent(String.class);
+        assertThat(result, is("Hello, Test!"));
     }
+
 }
